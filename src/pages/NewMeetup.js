dae2bc43495267c830e77300de536f7afc88eab9
Magde <valentinmagde@gmail.com>
2022-09-13 17:06:58 +0400

    store meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,25 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-45a54-default-rtdb.firebaseio.com/',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
+    );
 }
 
 export default NewMeetupPage
