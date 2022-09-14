420f767256509605652400a000f7204d85f2d708
Magde <valentinmagde@gmail.com>
2022-09-14 16:55:46 +0400

    finalize demo

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { FavoritesContextProvider } from './store/favorites-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <FavoritesContextProvider>
     <BrowserRouter>
-    <style jsx global>{`
-      body {
-        margin: 0px;
-        padding: 0px;
-      }
-    `}</style>
+      <style jsx global>
+        {`
+          body {
+            margin: 0px;
+            padding: 0px;
+          }
+        `}
+      </style>
         <App />
     </BrowserRouter>
+  </FavoritesContextProvider>
 );
