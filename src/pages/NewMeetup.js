9c06ce04121f0f994ce43de3fa436ee261e67cca
Valentinmagde <38159415+Valentinmagde@users.noreply.github.com>
2022-09-14 14:42:07 +0400

    store meetup in database (firebase)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,7 +3,7 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm"
 function NewMeetupPage() {
     function addMeetupHandler(meetupData) {
         fetch(
-            'https://react-getting-started-45a54-default-rtdb.firebaseio.com/',
+            'https://react-getting-started-45a54-default-rtdb.firebaseio.com/meetups.json',
             {
                 method: 'POST',
                 body: JSON.stringify(meetupData),
